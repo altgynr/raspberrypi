6bc58ba5d58bbff228d855cf70dd8cbe674faa35
altgynr <altug.yanar@hotmail.com>
2022-03-14 16:47:28 +0100

    update filezilla

--- filezilla.py
+++ filezilla.py
@@ -7,13 +7,13 @@ CRONOS_IP = '192.168.100.133'
 user = 'imc'
 pw = 'imc'
 cronos_dir = 'pcmcia/EMB_LRV_BRTB_10kN'
-destination_dir = '/home/pi/Documents'
+destination_dir = '/home/pi/github_altug/raspberrypi'
 
 def cronos_connect(CRONOS_IP,user,pw):
     try:
         ftp = ftplib.FTP(CRONOS_IP,user,pw)
         ftp.encoding = 'utf-8'
-        print ("Connection established!\n")
+        print ("Connection established!")
         return ftp
     except ftplib.error_perm as e:
         print("Connection could not be established! Check IP, user and password.")
@@ -23,25 +23,28 @@ def pi_mkdir(directory):
         oldmask = os.umask(000)
         os.makedirs(directory,mode=0o777)
         os.umask(oldmask)
-
-def send2cloud_rasp(source_folder):
-    sendString= 'blobxfer upload  --storage-account testbenchpi4 --sas "?sv=2020-08-04&ss=bfqt&srt=sco&sp=rwdlacupitfx&se=2023-08-09T17:30:05Z&st=2022-02-10T10:30:05Z&spr=https&sig=%2BkcIcyTpUK0tHDiSBW4gC%2FvZP%2BrvSa5YS2neC7X%2BnfM%3D" --remote-path achimpi4 --local-path /home/pi/Documents/pcmcia/EMB_LRV_BRTV_10kN'
+        
+def send2cloud():
+    account_name = 'cronos2pi'
+    account_key = 'dKjupim22gOrMOdYfY5cWZNDBzph3TJp+/qj6Z7JMZsXB7h/w31jRiZZ3owpiXhByzhMQ13eRAmbKrxrcdWwQw=='
+    remote_path = 'cronos-test/pcmcia'
+    local_path = '/home/pi/github_altug/raspberrypi/pcmcia/'
+    sendString = 'blobxfer upload --storage-account-key {} --storage-account {} --remote-path {} --local-path {}'.format(account_key, account_name, remote_path, local_path)
+    os.system(sendString)
     
-    folder="/home/pi/github/log_data/"
-    messfiles=makeFileList(folder)                 
-    print(messfiles)
-    for everyfile  in messfiles:
-        os.system(sendString+str(folder)+str(everyfile))
-    writeLogfile(messfiles)
+def send2cloud_achim():
+    local_path = '/home/pi/github_altug/raspberrypi/pcmcia/'
+    sendString= 'blobxfer upload  --storage-account testbenchpi4 --sas "?sv=2020-08-04&ss=bfqt&srt=sco&sp=rwdlacupitfx&se=2023-08-09T17:30:05Z&st=2022-02-10T10:30:05Z&spr=https&sig=%2BkcIcyTpUK0tHDiSBW4gC%2FvZP%2BrvSa5YS2neC7X%2BnfM%3D" --remote-path achimpi4/pcmcia --local-path '
+    os.system(sendString+local_path)
         
 def cronos2pi(cronos_dir,CRONOS_IP,user,pw):
     pi_mkdir(destination_dir + '/' + cronos_dir)
     ftp = cronos_connect(CRONOS_IP,user,pw)
     folders_cronos = ftp.nlst(cronos_dir)
     ftp.quit()
-    for i,folder in zip(range(2),folders_cronos):
-        print(folder)
+    for i,folder in zip(range(5),folders_cronos):
         ftp = cronos_connect(CRONOS_IP,user,pw)
+        print(folder)
         destination_folder = destination_dir + '/' + cronos_dir + '/' + folder
         pi_mkdir(destination_folder)
         ftp.cwd(cronos_dir + '/' + folder)
@@ -54,25 +57,15 @@ def cronos2pi(cronos_dir,CRONOS_IP,user,pw):
                 local_filepath = os.path.join(destination_folder, filename)
                 lf = open(local_filepath, "wb")
                 ftp.retrbinary("RETR " + filename, lf.write)
-                lf.close()
-        #send folder to cloud
-        #sendString = 'blobxfer upload --storage-account-key dKjupim22gOrMOdYfY5cWZNDBzph3TJp+/qj6Z7JMZsXB7h/w31jRiZZ3owpiXhByzhMQ13eRAmbKrxrcdWwQw== --storage-account cronos2pi --remote-path cronos-test/pcmcia --local-path /home/pi/Documents/pcmcia/' 
-        sendString = 'blobxfer upload --storage-account testbenchpi4 --sas "?sv=2020-08-04&ss=bfqt&srt=sco&sp=rwdlacupitfx&se=2023-08-09T17:30:05Z&st=2022-02-10T10:30:05Z&spr=https&sig=%2BkcIcyTpUK0tHDiSBW4gC%2FvZP%2BrvSa5YS2neC7X%2BnfM%3D" --remote-path "achimpi4" --local-path "/home/pi/Documents/pcmcia"'
-        os.system(sendString)
-        print("Download finished!\n")
+                lf.close()   
         ftp.quit()
+    print("Raspi upload finished!\n")
+    #send folder to cloud
+    send2cloud_achim()
+    print("Cloud upload finished!\n")
     
 cronos2pi(cronos_dir,CRONOS_IP,user,pw)
 sys.exit()
-        
-# from azure.storage.blob import BlockBlobService
-# 
-# container = 'cronos-test'
-# block_blob_service = BlockBlobService(account_name='cronos2pi', account_key='dKjupim22gOrMOdYfY5cWZNDBzph3TJp+/qj6Z7JMZsXB7h/w31jRiZZ3owpiXhByzhMQ13eRAmbKrxrcdWwQw==')
-# content = block_blob_service.list_blobs(container)
-# blobcontentList = []
-# for blob in content:
-#     blobcontentList.append(blob.name)
 
 
 
